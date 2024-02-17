5daa9d779529d97a4773f83fb5e5bac33bd85134
George Maia <georgemaiaf@gmail.com>
2024-02-17 16:07:31 -0300

    refactor: :recycle: tag removal

--- src/_components/postFlatData.tsx
+++ src/_components/postFlatData.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import Image from 'next/image';
 import { Box, Typography } from '@mui/material';
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
